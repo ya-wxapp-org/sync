6b485ef841de357437154bebf657aaaa0fb7293b
泽荣 <foru.fy@gmail.com>
2018-03-21 10:36:27 +0800

    fix issue

--- index.js
+++ index.js
@@ -3,50 +3,57 @@ const Sync = {
   cache: {}
 };
 
-const PREFIX = '__YA_WXAPP_GROUP_SYNC__'
+const PREFIX = '__YA_WXAPP_GROUP_SYNC__';
 
 function prefix(key) {
-    return PREFIX + key
+  return PREFIX + key;
 }
 
 function getter(key, isJson) {
-    return function () {
-        if (!Sync.cache[key]) {
-            // 这里可以用其他的存储器
-            const theCache = wx.getStorageSync(prefix(key))
-            Sync.cache[key] = isJson ? JSON.parse(theCache) : theCache
+  return function() {
+    if (!Sync.cache[key]) {
+      // 这里可以用其他的存储器
+      let theCache = wx.getStorageSync(prefix(key));
+      if (isJson) {
+        try {
+          theCache = JSON.parse(theCache);
+        } catch (_e) {
+          theCache = null;
         }
-        return Sync.cache[key]
+      }
+      Sync.cache[key] = theCache;
     }
+    return Sync.cache[key];
+  };
 }
 
 function setter(key, isJson) {
-    return function (val) {
-        if (isJson) val = JSON.stringify(val)
-        wx.setStorageSync(prefix(key), val)
-        Sync.cache[key] = val
-    }
+  return function(val) {
+    if (isJson) val = JSON.stringify(val);
+    wx.setStorageSync(prefix(key), val);
+    Sync.cache[key] = val;
+  };
 }
 
 function prop(key, isJson) {
-    return {
-      get: getter(key, isJson),
-      set: setter(key, isJson)
-    }
+  return {
+    get: getter(key, isJson),
+    set: setter(key, isJson)
+  };
 }
 
 export function register(key, isJson = false) {
   Sync.registers.push(key);
-  Object.defineProperty(Sync, key, prop(key, isJson))
+  Object.defineProperty(Sync, key, prop(key, isJson));
 }
 
 export function remove(key) {
-    delete Sync.registers[key]
-    return wx.removeStorageSync(prefix(key))
+  delete Sync.registers[key];
+  return wx.removeStorageSync(prefix(key));
 }
 
 export function clear() {
-    Sync.registers.forEach(register => remove(register))
+  Sync.registers.forEach(register => remove(register));
 }
 
 export default Sync;
